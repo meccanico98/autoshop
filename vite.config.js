32612ab87526ebcfe645c9d9330a685a09a3022f
meccanico98 <veronawork@proton.me>
2026-06-05 15:23:41 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,16 @@ import react from '@vitejs/plugin-react'
  
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          charts: ['recharts'],
+          excel: ['xlsx'],
+        }
+      }
+    }
+  }
 })
